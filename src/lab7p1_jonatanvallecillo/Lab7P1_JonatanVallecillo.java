9199c05566720cf92c376f905f530dbf5622c6df
Jonatan V. <jonatan.vallecillo123@gmail.com>
2024-03-01 15:05:22 -0600

    commit 2

--- src/lab7p1_jonatanvallecillo/Lab7P1_JonatanVallecillo.java
+++ src/lab7p1_jonatanvallecillo/Lab7P1_JonatanVallecillo.java
@@ -34,56 +34,79 @@ public class Lab7P1_JonatanVallecillo {
             
             switch(opcion){
                 case 1:
-                    System.out.print("Ingrese la cantidad de filas: ");
+                    System.out.print("\nIngrese la cantidad de filas: ");
                     int fila = leer.nextInt();
                     System.out.print("Ingrese la cantidad de columnas: ");
                     int columna = leer.nextInt();
                     System.out.println();
                     
-                    int [][] matriz = matriz(fila,columna);
-                    imprimir(matriz);
+                    int [][] matriz = new int [fila][columna];
+                    matriz = generarMatriz(matriz);
+                    imprimirMatriz(matriz);
                     PuntoEquilibrio(matriz);
                     
                     System.out.println();
                     break;
                     
                 case 2: 
-                    System.out.print("Ingrese las filas: ");
+                    System.out.print("\nIngrese las filas: ");
                     fila = leer.nextInt();
                     System.out.print("Ingrese las columnas: ");
                     columna = leer.nextInt();
                     System.out.println();
                     
-                    matriz = matriz(fila,columna);
-                    imprimir(matriz);
+                    matriz = new int [fila][columna];
+                    matriz = generarMatriz(matriz);
+                    imprimirMatriz(matriz);
+                    
+                    NumSolitario(matriz);
+                    
+                    System.out.println();
                     
                     break; 
+                    
+                case 3:
+                    System.out.print("\nIngrese el tamano: ");
+                    int tamaño = leer.nextInt();
+                    
+                    matriz = new int [tamaño][tamaño];
+                    matriz = generarMatriz(matriz);
+                    imprimirMatriz(matriz);
+                    
+                    
+                    System.out.println("Ingrese comando de rotacion: ");
+                    String rotacion = leer1.nextLine();
+                    matrizRotada(matriz, rotacion);
+                    
+                    System.out.println();
+                    break;
+                    
                 case 4:
                     menu = 0;
+                    break;
             }
         }
     }
     
-    public static int [][] matriz(int fila, int columna){
-        int [][] temporal = new int [fila][columna];
-        int random = 0;
-        for(int i = 0; i < temporal.length; i ++){
-            for(int j = 0; j < temporal[i].length; j++){
-                random = rand.nextInt(10);
-                temporal[i][j] = random;
+    public static int[][] generarMatriz(int[][] matriz) {//Ejercicio 1 y 2
+        Random ran = new Random();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = rand.nextInt(5) + 2;
             }
         }
-        return temporal;
-    } //Ejercicio 1
+        return matriz;
+    } 
     
-    public static void imprimir(int [][] matriz){
-        for(int i = 0; i < matriz.length; i++){
-            for(int j = 0; j < matriz[i].length; j++){
-                System.out.print("["+matriz[i][j]+"]");
+    public static void imprimirMatriz(int[][] matriz) { //Ejercicio 1 y 2
+    System.out.println("");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+             System.out.print("[" + matriz[i][j] + "] ");
             }
-            System.out.println();
+        System.out.println("");
         }
-    } //Ejercicio 1
+    }
     
     public static void PuntoEquilibrio(int [][] matriz){
         int P_equilibrio1 = 0;
@@ -104,7 +127,7 @@ public class Lab7P1_JonatanVallecillo {
                     for(int v = 0; v < matriz[i].length; v++){
                         if(u == i && v != j){
                             sumaF += matriz[u][v];
-                        }else if(u != 0 && v == j){
+                        }else if(u != i && v == j){
                             sumaC += matriz[u][v];
                         }
                     }
@@ -124,4 +147,63 @@ public class Lab7P1_JonatanVallecillo {
                 + "Porque su suma en horizontal es "+sumaF2+" y su suma en vertical es "+sumaC2);
     } //Ejericio 1
     
+    public static void NumSolitario(int [][] matriz){ //Ejercicio 2
+        int cantidad = 0;
+        int sumaF = 0;
+        int sumaC = 0;
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                cantidad = matriz[i][j];
+                int cantidad2 = cantidad;
+                
+                for(int u = 0; u < matriz.length; u++){
+                    for(int v = 0; v < matriz[i].length; v++){
+                        if(u == i && v != j && cantidad2 > 0){
+                            cantidad2 --;
+                        }else if(u != i && v == j){
+                            if(cantidad - v >= 0){
+                                sumaC += matriz[u][v];
+                            }else{
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public static int [][] matrizRotada(int [][] matriz, String rotacion){
+        int [][] temporal = new int [matriz.length][matriz.length];
+        char carac = ' ';
+        
+        for(int i = 0; i < rotacion.length(); i++){
+            int dimension1 = 0;
+            int dimension2 = 0;
+            int pos1 = 0;
+            int posy = 0;
+            carac = rotacion.charAt(i);
+            if(carac == 'f' && i == 0){
+                dimension1 = 1;
+                pos1 = rotacion.charAt(i+1);
+            }else if(carac == 'c' && i == 0){
+                dimension1 = 0;
+                pos1 = rotacion.charAt(i+1);
+            }else if(carac == 'f'){
+                dimension2 = 1;
+            }else if(carac == 'c'){
+                dimension2 = 0;
+            }
+            
+        }
+        
+        for(int i = 0; i < temporal.length; i++){
+            for(int j = 0; j < temporal[i].length; j++){
+                
+            }
+        }
+        
+        return temporal;
+    }
+        
 }
